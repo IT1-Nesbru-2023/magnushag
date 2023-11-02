42cc70257982129fa7586376fcb083157fd4019b
Magnus Hægh <144103123+Magnushag@users.noreply.github.com>
2023-11-02 14:13:14 +0100

    Add files via upload

---
+++ Timer/uke44.js
@@ -0,0 +1,16 @@
+let tall = Math.random();
+
+console.log(tall);
+
+let tall2 = Math.random() * 10;
+
+console.log(tall2);
+
+let tall3 = Math.floor(Math.random() * 11);
+
+console.log(tall3);
+
+let tall4 = Math.floor(Math.random()* 91 + 10)
+
+console.log(tall4)
+
