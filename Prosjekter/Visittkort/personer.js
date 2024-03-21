d18c976d845834773938fe3d9d4cecc94205acd5
Magnus Hægh <144103123+Magnushag@users.noreply.github.com>
2024-03-21 14:05:49 +0100

    Add files via upload

---
+++ Prosjekter/Visittkort/personer.js
@@ -0,0 +1,52 @@
+let personer = {
+  kortArray: [
+    {
+      navn: "Erna Solberg",
+      kjonn: "Dame",
+      stilling: "Husmor",
+      adresse: "Solberggsgate 11",
+      telefonnummer: "12345678",
+      email: "Erna@solberg.com",
+      dato: "21.03.2024",
+      bilde:
+        "https://www.stortinget.no/Personimages/PersonImages_ExtraLarge/ES_ekstrastort.jpg",
+      farge: "black",
+    },
+    {
+      navn: "Morgan",
+      kjonn: "Mann",
+      stilling: "Konsulent",
+      adresse: "Morgansvei 2",
+      telefonnummer: "99999999",
+      email: "Morgan@bcg.com",
+      dato: "21.03.2024",
+      bilde:
+        "https://inst.eecs.berkeley.edu/~cs194-26/fa21/upload/files/proj3/cs194-26-aad/out/gigachad/0.jpg",
+      farge: "white",
+    },
+    {
+      navn: "Sindre Finnes",
+      kjonn: "Mann",
+      stilling: "proff innsidehander",
+      adresse: "Solberggsgate 11",
+      telefonnummer: "87654321",
+      email: "Finnes@aksjer.com",
+      dato: "21.03.2024",
+      bilde:
+        "https://premium.vgc.no/v2/images/4a22822c-f39e-4020-8875-e3fc6d991a43?fit=crop&format=auto&h=1267&w=1900&s=2571e99212a556604ab63f6af40e87fd8f860737",
+      farge: "Black",
+    },
+    {
+      navn: "Magnus Hægh",
+      kjonn: "Mann",
+      stilling: "Student",
+      adresse: "Jon smørs vei 11",
+      telefonnummer: "11111111",
+      email: "Haegh@gmail.com",
+      dato: "21.03.2024",
+      bilde:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOK4m3icy-TWIPyDXFu1zwft-FBWED5TEJSg&usqp=CAU",
+      farge: "white",
+    },
+  ],
+};
